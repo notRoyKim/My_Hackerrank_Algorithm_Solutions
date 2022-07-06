fc4ae987c5cb05ab0f283ecca9403cefe9581fcd
NRK <49558826+notRoyKim@users.noreply.github.com>
2022-07-06 16:55:45 +0900

    Create Maximum Perimeter Triangle.java

---
+++ Greedy/Maximum Perimeter Triangle.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'maximumPerimeterTriangle' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY sticks as parameter.
+     */
+
+    public static List<Integer> maximumPerimeterTriangle(List<Integer> sticks) {
+    // Write your code here
+        
+        Collections.sort(sticks, Collections.reverseOrder());
+    
+        for(int i = 0; i < sticks.size()-2; i++) {
+            if(sticks.get(i) >= sticks.get(i+1) + sticks.get(i+2)) {
+                continue;
+            }
+            
+            List<Integer> result = sticks.subList(i, i+ 3);
+            Collections.sort(result);
+            return result;
+        }
+            return Arrays.asList(-1);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> sticks = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.maximumPerimeterTriangle(sticks);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
